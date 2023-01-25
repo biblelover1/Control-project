04cb924df629be101642aa13a78d98152c0ba106
Evgeniy <evgenijkopylov19540@gmail.com>
2023-01-25 20:29:58 +0300

    added program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+string [] words = {"hello", "world", "abc", "bcd"};
+for(int i = 0; i < words.Length; i++)
+{
+    string s = words[i];
+    char [] arr = s.ToCharArray();
+    int length = arr.Length;
+    if(length<=3)
+    {
+     Console.WriteLine(s);
+    }
+}
